1261cf9c8f6bfa3c868542cb0ebb8389c062f1e1
YehorCherednychenko <20003000m76@gmail.com>
2021-06-22 12:00:05 +0200

    Unpacked css and js files

--- static/dashboard.php
+++ static/dashboard.php
@@ -15,7 +15,7 @@
 	<meta name="author" content="AdminKit">
 	<meta name="keywords"
 		content="adminkit, bootstrap, bootstrap 5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
-
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link rel="shortcut icon" href="img/icons/icon-48x48.png" />
 
