bc743985a97c3d1028690097abe1136714593e10
YehorCherednychenko <20003000m76@gmail.com>
2021-06-23 15:14:58 +0200

    add event button

--- static/dashboard.php
+++ static/dashboard.php
@@ -43,7 +43,22 @@
 			
 		}
 	</script>
-	
+
+	<script>
+		function addEvent()
+		{
+			$("#calendar").evoCalendar('addCalendarEvent', [
+				{
+				  id: '09nk7Ts',
+				  name: "My Birthday",
+				  date: "February/15/2020",
+				  type: "birthday",
+				  everyYear: true
+				}
+			  ]);
+			
+		}
+	</script>
 </head>
 
 <body>
@@ -238,7 +253,7 @@
 							</style>
 						</div>
 					</div>
-
+										<a class="add_event_button" onclick="addEvent()">Add event</a>
 										<div class="row" id="calendar"></div>
 										<script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js"></script>
 										<script src="js/evo-calendar.min.js"></script>
@@ -264,7 +279,40 @@
 																		description: "Vacation leave for 3 days.", // Event description (optional)
 																		type: "event",
 																		color: "#63d867" // Event custom color (optional)
-																	}
+																	},
+																	{
+																		name: "Vacation Leave",
+																		badge: "02/13 - 02/15", // Event badge (optional)
+																		date: ["February/13/2020", "February/15/2020"], // Date range
+																		description: "Vacation leave for 3 days.", // Event description (optional)
+																		type: "event",
+																		color: "#63d867" // Event custom color (optional)
+																	},
+																	{
+																		name: "Vacation Leave",
+																		badge: "02/13 - 02/15", // Event badge (optional)
+																		date: ["February/13/2020", "February/15/2020"], // Date range
+																		description: "Vacation leave for 3 days.", // Event description (optional)
+																		type: "event",
+																		color: "#63d867" // Event custom color (optional)
+																	},
+																	{
+																		name: "Vacation Leave",
+																		badge: "02/13 - 02/15", // Event badge (optional)
+																		date: ["February/13/2020", "February/15/2020"], // Date range
+																		description: "Vacation leave for 3 days.", // Event description (optional)
+																		type: "event",
+																		color: "#63d867" // Event custom color (optional)
+																	},
+																	{
+																		name: "Vacation Leave",
+																		badge: "02/13 - 02/15", // Event badge (optional)
+																		date: ["February/13/2020", "February/15/2020"], // Date range
+																		description: "Vacation leave for 3 days.", // Event description (optional)
+																		type: "event",
+																		color: "#63d867" // Event custom color (optional)
+																	},
+																	
 																]
 
 															})
