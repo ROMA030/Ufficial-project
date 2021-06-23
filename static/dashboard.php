c054a5dcdd4fb8708206f0e8a8b497c0952810f3
YehorCherednychenko <20003000m76@gmail.com>
2021-06-23 16:18:45 +0200

    a add event changes

--- static/dashboard.php
+++ static/dashboard.php
@@ -253,9 +253,56 @@
 							</style>
 						</div>
 					</div>
-										<a class="add_event_button" onclick="addEvent()" style="text-decoration: none;" onmouseover = "this.style.background = '#fff'" onmouseout  = "this.style.background = '#8f0000'">Add event</a>
-										<div class="row" id="calendar"></div>
+										
+										<a class="add_event_button" onclick="document.getElementById('id01').style.display='block'" style="text-decoration: none;" onmouseover = "this.style.background = '#fff'" onmouseout  = "this.style.background = '#8f0000'">Add event</a>
+										
+										<div id="id01" class="modal">
+										  <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+										  <form class="modal-content" action="/action_page.php">
+											<div class="container">
+											  <h1>Sign Up</h1>
+											  <p>Please fill in this form to create an account.</p>
+											  <hr>
+											  <label for="email"><b>Email</b></label>
+											  <input type="text" placeholder="Enter Email" name="email" required>
+
+											  <label for="psw"><b>Password</b></label>
+											  <input type="password" placeholder="Enter Password" name="psw" required>
+
+											  <label for="psw-repeat"><b>Repeat Password</b></label>
+											  <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+      
+											  <label>
+												<input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
+											  </label>
+
+											  <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+
+											  <div class="clearfix">
+												<button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+												<button type="submit" class="signupbtn">Sign Up</button>
+											  </div>
+											</div>
+										  </form>
+										</div>
+
 										<script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js"></script>
+										
+										<script>
+										// Get the modal
+										var modal = document.getElementById('id01');
+
+										// When the user clicks anywhere outside of the modal, close it
+										window.onclick = function(event) {
+										  if (event.target == modal) {
+											modal.style.display = "none";
+										  }
+										}
+										</script>
+										
+										
+										<div class="row" id="calendar"></div>
+										
 										<script src="js/evo-calendar.min.js"></script>
 
 										<script>
