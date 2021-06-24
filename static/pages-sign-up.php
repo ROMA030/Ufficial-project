14ec70ed1f707627dc3de9f4f9c1ab7472b4ca9e
FrancescoTonassi <57535330+FrancescoTonassi@users.noreply.github.com>
2021-06-24 11:42:45 +0200

    ADDED every error for sign up and reset password

--- static/pages-sign-up.php
+++ static/pages-sign-up.php
@@ -1,3 +1,7 @@
+<?php
+	session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -34,7 +38,7 @@
 						<div class="card">
 							<div class="card-body">
 								<div class="m-sm-4">
-									<form method="post" enctype="multipart/form-data" action="php/register_user.php">
+									<form method="post" enctype="multipart/form-data">
 										<div class="mb-3">
 											<label class="form-label">Nome:</label>
 											<input class="form-control form-control-lg" type="text" name="name" placeholder="Enter your name" required/>
@@ -75,6 +79,51 @@
 											<button type="submit" name="submit" value="Submit" class="btn btn-lg btn-primary">Sign up</button>
 										</div>
 									</form>
+									<?php
+		$conn = mysqli_connect("localhost","root","","xeos");
+		if (mysqli_connect_errno()) {
+			echo "Failed to connect to MySQL: " . mysqli_connect_error();
+			exit();
+		}
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+			$name = $_POST["name"];
+			$surname = $_POST["surname"];
+			$email = $_POST["email"];
+			$username = $_POST["username"];
+			$password = $_POST["password"];
+			$usertype = $_POST['role'];
+			$securityAnswer = $_POST['securityAnswer'];
+
+			$avatar = addslashes(file_get_contents($_FILES['avatar']['tmp_name']));
+
+			// If forum inputs aren't empty 
+			if (!empty($name) && !empty($surname) && !empty($email) && !empty($username) && !empty($password) && !empty($securityAnswer)) {
+												
+				// Validate password strength
+				$uppercase = preg_match('@[A-Z]@', $password);
+				$lowercase = preg_match('@[a-z]@', $password);
+				$number = preg_match('@[0-9]@', $password);
+				$specialChars = preg_match('@[^\w]@', $password);
+
+				if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8) {
+					//echo "<p class='text-danger text-center mt-3'>Password should be at least 8 characters in length and should include at least one upper case letter, one number, and one special character</p>";
+					echo "<h5 class='notification text-danger mt-3'>Password should be at least 8 characters in length and should include at least one upper case letter, one number, and one special character</h5>";             											
+				}else{
+					$checkUser = "SELECT * FROM users WHERE Username = '$username'";
+					$checkResult = mysqli_query($conn, $checkUser);
+					// If number of rows in result variable is major than 0
+					if (mysqli_num_rows($checkResult) > 0) {
+						echo "<h5 class='notification text-danger mt-3'>Username already exists</h5>";                  
+					}else {
+						$users = "INSERT INTO users(Name, Surname, Email, Username, Password , Avatar, UserType, Risposta) VALUES ('$name', '$surname', '$email', '$username', '$password' , '$avatar', '$usertype','$securityAnswer')";
+						$result = mysqli_query($conn, $users);
+						header("location: ../pages-sign-in.php");
+					}
+					mysqli_close($conn);
+				}
+			}                
+		}
+		?>
 								</div>
 							</div>
 						</div>
@@ -89,6 +138,7 @@
 
 		<script src="js/app.js"></script>
 
+		
 	</body>
 
 </html>
