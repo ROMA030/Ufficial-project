b1518af1f1ac9d96cec88ad3d6db71c13f5447cd
YehorCherednychenko <20003000m76@gmail.com>
2021-06-25 09:28:27 +0200

    deleted analytics

--- static/pages-profile.php
+++ static/pages-profile.php
@@ -77,8 +77,6 @@
 							<div class="dropdown-menu dropdown-menu-end">
 								<a class="dropdown-item" href="pages-profile.php"><i class="align-middle me-1"
 										data-feather="user"></i> Profile</a>
-								<a class="dropdown-item" href="#"><i class="align-middle me-1"
-										data-feather="pie-chart"></i> Analytics</a>
 								<div class="dropdown-divider"></div>
 								<a class="dropdown-item" href="Contact.php"><i class="align-middle me-1"
 										data-feather="help-circle"></i> Contact</a>
