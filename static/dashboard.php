6e1cab93ce88d49ad3461467ad81581f0e23bbb0
YehorCherednychenko <20003000m76@gmail.com>
2021-06-25 09:35:05 +0200

    footer

--- static/dashboard.php
+++ static/dashboard.php
@@ -375,9 +375,31 @@
 						</div>
 					</div>
 				</div>
-
 			</main>
-
+			<footer class="footer">
+				<div class="container-fluid">
+					<div class="row text-muted">
+						<div class="col-6 text-start">
+							<p class="mb-0">
+								<a class="text-muted" target="_blank"><strong>Xeos</strong></a> &copy;
+							</p>
+						</div>
+						<div class="col-6 text-end">
+							<ul class="list-inline">
+								<li class="list-inline-item">
+									<a class="text-muted" target="_blank" href="index.html#contact">Contact</a>
+								</li>
+								<li class="list-inline-item">
+									<a class="text-muted" href="https://adminkit.io/" target="_blank">Privacy</a>
+								</li>
+								<li class="list-inline-item">
+									<a class="text-muted" href="https://adminkit.io/" target="_blank">Terms</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</footer>
 		</div>
 	</div>
 
