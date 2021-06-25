f2b0b5b53893bd5e57fc6d7e90048e8d3ab515eb
YehorCherednychenko <20003000m76@gmail.com>
2021-06-25 09:30:12 +0200

    asd

--- static/clubs.php
+++ static/clubs.php
@@ -83,8 +83,7 @@
 								<a class="dropdown-item" href="pages-profile.html"><i class="align-middle me-1"
 										data-feather="user"></i> Profile</a>
 								<div class="dropdown-divider"></div>
-								<a class="dropdown-item" href="pages-settings.html"><i class="align-middle me-1"
-										data-feather="settings"></i> Settings & Privacy</a>
+								
 								<a class="dropdown-item" href="#"><i class="align-middle me-1"
 										data-feather="help-circle"></i> Help Center</a>
 								<div class="dropdown-divider"></div>
